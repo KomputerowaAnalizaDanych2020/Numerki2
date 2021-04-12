6802d1c28fd06d072e4ebd99263bc526142dcd4b
maldo31 <74554376+maldo31@users.noreply.github.com>
2021-04-13 01:40:47 +0200

    smiga i bucy

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 
 def matrix_create(matrix_given):
-    x_dim=len(matrix_given)
-    y_dim=len(matrix_given[0])
+    x_dim = len(matrix_given)
+    y_dim = len(matrix_given[0])
     matrix_of_zeros = [[0 for col in range(y_dim)] for row in range(x_dim)]
-    """row=[]
-    for i in range(y_dim):
-        row.append(0)
-    matrix_of_zeros=[]
-    for i in range(x_dim):
-        matrix_of_zeros.append(row)
-    """
     return matrix_of_zeros
+
+
 def equ_and_res(given_matrix):
-    results=[]
-    equations=[]
+    results = []
+    equations = []
     for row in given_matrix:
         results.append(row.pop())
         equations.append(row)
     return equations, results
-def add_matrixes(A,B):
-    result_matrix=matrix_create(A)
-    for row in range (len(A)):
-        for col in range (len(A[0])):
-            result_matrix[row][col]=A[row][col]+B[row][col]
+
+
+def add_matrixes(A, B):
+    result_matrix = matrix_create(A)
+    for row in range(len(A)):
+        for col in range(len(A[0])):
+            result_matrix[row][col] = A[row][col]+B[row][col]
     return result_matrix
+
+
 def matrix_minus_one_pow(matrix):
-    for row in range (len(matrix)):
-        for col in range (len(matrix[0])):
-            if matrix[row][col]!=0:
-                matrix[row][col]=1/matrix[row][col]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] != 0:
+                matrix[row][col] = 1/matrix[row][col]
     return matrix
+
+
 def MDivide(matrix_to_split):
     L = matrix_create(matrix_to_split)
     D = matrix_create(matrix_to_split)
@@ -42,7 +43,9 @@ def MDivide(matrix_to_split):
                 L[row][col] = matrix_to_split[row][col]
             elif col > row:
                 U[row][col] = matrix_to_split[row][col]
-    return L,D,U
+    return L, D, U
+
+
 def multiply_matrixes(A,B):
     multiplied = [[0 for x in range(len(A[0]))] for y in range(len(B))]
 
@@ -51,71 +54,77 @@ def multiply_matrixes(A,B):
             for k in range(len(B)):
                 multiplied[i][j] -= A[i][k] * B[k][j]
     return multiplied
-def matrix_vector(A,B):
+
+
+def matrix_vector(A, B):
     vector=[0]*len(A)
     for i in range(len(A)):
         for j in range(len(A[0])):
-            vector[i]+=B[i]*A[i][j]
+            vector[i] += B[i]*A[i][j]
     return vector
 
 
-
 def get_result(M,res):
-    arguments=[0]*len(M)
-    for i in range (len(M)):
-        x=qmat[i]
-        for col in range (len(M[i])):
+    arguments = [0]*len(M)
+    for i in range(len(M)):
+        x = qmat[i]
+        for col in range(len(M[i])):
             if M[i][col] != 0:
-                x+=M[i][col]*arguments_list[col]
-        arguments[i]=x
+                x += M[i][col]*arguments_list[col]
+        arguments[i] = x
     return arguments
 
+
 def check_dominant(matrix_to_check):
-    columns=[0]*len(matrix_to_check[0])
+    columns = [0]*len(matrix_to_check[0])
     rows = [0] * len(matrix_to_check)
 
     for i in range(len(matrix_to_check)):
         for j in range (len(matrix_to_check[0])):
-            if (i!=j):
-                rows[i]+=matrix_to_check[i][j]
-                print(rows)
-                columns[j]+=matrix_to_check[i][j]
+            if i != j:
+                rows[i] += matrix_to_check[i][j]
+                columns[j] += matrix_to_check[i][j]
     for i in range(len(matrix_to_check)):
-        if (matrix_to_check[i][i]<columns[i]):
-            print("kolumna "+str(i))
-
+        if matrix_to_check[i][i]<columns[i]:
             return False
-        if (matrix_to_check[i][i] < rows[i]):
-            print("wiersz "+str(i))
-            print(matrix_to_check[i][i])
-            print(rows[i])
+        if matrix_to_check[i][i] < rows[i]:
             return False
     return True
+
+
 def delta(arguments,expected,equations):
-    arg
+    errors = 0
+    for row in range(len(equations)):
+        actual = 0
+        for column in range(len(equations)):
+            actual += equations[row][column]*arguments[column]
+        errors += abs(abs(actual)-abs(expected[row]))
+    return errors
 
 print("Rozwiazywanie ukladu N rownan liniowych z N niewiadomymi za pomoca metody iteracyjnej Jacobiego")
-# Podanie funkcji jako odczyt z pliku
 print("Wybierz kryterium zatrzymania:")
 print("1. Spelnienie warunku nalozonego na dokladnosc")
 print("2. Osiagniecie zadanej liczby iteracji")
 stopChoice = input()
-if stopChoice == 1:
-    epsilon = input("Podaj epsilon: ")
-if stopChoice == 2:
-    counter = input("Podaj liczbe iteracji: ")
+counter = 10000000
+epsilon=0.0000000000000001
+if stopChoice == "1":
+    epsilon = float(input("Podaj epsilon: "))
+if stopChoice == "2":
+    counter = int(input("Podaj liczbe iteracji: "))
 with open('macierz.txt', 'r') as f:
     matrix = [[float(num) for num in line.split(',')] for line in f]
 equ, res = equ_and_res(matrix)
-if(check_dominant(equ)):
+if check_dominant(equ):
     arguments_list = [0] * len(matrix)
     lmat, dmat, umat = MDivide(equ)
     added = add_matrixes(lmat, umat)
     matrix_minus_one_pow(dmat)
     resultmat = multiply_matrixes(dmat, added)
     qmat = matrix_vector(dmat, res)
-    for i in range(5):
+    while counter > 0 and epsilon < delta(arguments_list, res, equ):
         arguments_list = get_result(resultmat, res)
+        counter -= 1
     print(arguments_list)
 else:
     print("Macierz nie jest macierza dominującą przekątniowo")
